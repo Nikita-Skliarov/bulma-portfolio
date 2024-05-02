e5341bb59f9282e954f3334bf6524e082d9b607a
Nikita-Skliarov <0357026@student.rocvantwente.nl>
2024-05-03 00:35:34 +0200

    Add some custom CSS styles for arrow button

--- index.php
+++ index.php
@@ -70,9 +70,9 @@
       <div class="container">
         <h1 class="title">Welcome to my Portfolio. Explore a showcase of my passion, creativity, and
           dedication to IT.</h1>
-        <a href="#about" class="button is-large is-rounded is-primary is-outlined">
+        <a href="#about" class="button is-large is-rounded is-primary is-outlined mt-5" id="button-around-chevron">
           <span class="icon is-small">
-            <i class="fas fa-chevron-down"></i>
+            <i class="fas fa-chevron-down" id="chevron"></i>
           </span>
         </a>
       </div>
