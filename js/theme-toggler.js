c677796938685156e6f3ad3f84b755684babb944
Nikita-Skliarov <0357026@student.rocvantwente.nl>
2024-05-03 01:35:55 +0200

    Add animation of switching themes

--- js/theme-toggler.js
+++ js/theme-toggler.js
@@ -1,21 +1,33 @@
- // Function to toggle between light and dark mode
- function toggleTheme() {
-    const htmlElement = $('html');
-    if (htmlElement.attr('data-theme') === 'dark') {
-      htmlElement.attr('data-theme', 'light');
-      $('#theme-toggle i').removeClass('fa-moon').addClass('fa-lightbulb');
-    } else {
-      htmlElement.attr('data-theme', 'dark');
-      $('#theme-toggle i').removeClass('fa-lightbulb').addClass('fa-moon');
-    }
-  }
+// Function to toggle between light and dark mode
+function toggleTheme() {
+  const htmlElement = $('html');
+  if (htmlElement.attr('data-theme') === 'dark') {
+    htmlElement.attr('data-theme', 'light');
+    $('#theme-toggle i').addClass('sun-moon-icon');
+    // Wait 500ms before animation to complete
+    setTimeout(() => {
+      $('#theme-toggle i').removeClass('sun-moon-icon');
+    }, 500);
+    $('#theme-toggle i').removeClass('fa-moon').addClass('fa-lightbulb');
 
-  // Event listener for theme toggle button
-  $('#theme-toggle').on('click', function () {
-    toggleTheme();
-  });
-
-  // Detect user's theme and update the toggle button icon accordingly
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  } else {
+    htmlElement.attr('data-theme', 'dark');
+    $('#theme-toggle i').addClass('sun-moon-icon');
+    // Wait 500ms before animation to complete
+    setTimeout(() => {
+      $('#theme-toggle i').removeClass('sun-moon-icon');
+    }, 500);
     $('#theme-toggle i').removeClass('fa-lightbulb').addClass('fa-moon');
+
   }
+}
+
+// Event listener for theme toggle button
+$('#theme-toggle').on('click', function () {
+  toggleTheme();
+});
+
+// Detect user's theme and update the toggle button icon accordingly
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  $('#theme-toggle i').removeClass('fa-lightbulb').addClass('fa-moon');
+}
